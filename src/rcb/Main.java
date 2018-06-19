0e0b06f13f37a91f280e5e04241262c9d47d5bfc
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-06-19 10:36:40 -0300

    after of this commit remember do a git reset hard

--- src/rcb/Main.java
+++ src/rcb/Main.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final int MAX=50000;
+    private static final int STEP=1000;
+
     public static void main(String[] args) throws IOException {
+        String path;
         if (args.length == 1) {
-            File file = new File(args[0]);
-            BufferedInputStream bf = new BufferedInputStream(new FileInputStream(file));
-            Scanner sc = new Scanner(bf);
+            for (int j = STEP; j < MAX; j+=STEP) {
+                path = args[0];
+                File file = new File(path.concat("case-" + j + ".txt"));
+                BufferedInputStream bf = new BufferedInputStream(new FileInputStream(file));
+                Scanner sc = new Scanner(bf);
 
-            int size = Integer.parseInt(sc.nextLine());
-            int[] vector = new int[size];
+                int size = Integer.parseInt(sc.nextLine());
+                int[] vector = new int[size];
 
-            for (int i = 0; i < size; i++) {
-                vector[i] = Integer.parseInt(sc.nextLine());
+                for (int i = 0; i < size; i++) {
+                    vector[i] = Integer.parseInt(sc.nextLine());
+                }
+
+                int resBrute = MaxSum.bruteForce(vector, size);
+                int resDivide = MaxSum.divideAndConquer(vector, 0, size - 1);
+                System.out.println("Case: " + j);
+                System.out.println("\tBrute Force: " + resBrute);
+                System.out.println("\tDivide and Conquer: " + resDivide);
+                System.out.println();
             }
-
-            int resBrute = MaxSum.bruteForce(vector, size);
-            int resDivide = MaxSum.divideAndConquer(vector, 0, size - 1);
-            System.out.println("Brute Force: " + resBrute);
-            System.out.println("Divide and Conquer: " + resDivide);
         } else {
             System.err.println("\uD83C\uDFB7 Invalid parameters.");
         }
+
     }
 }
