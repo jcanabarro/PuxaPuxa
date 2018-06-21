e742e13464309dcacc09b65fe05490166f40e07a
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-06-21 13:46:17 -0300

    fix printing

--- src/rcb/Main.java
+++ src/rcb/Main.java
@@ -13,45 +13,54 @@ public class Main {
 
     private static final String DIVIDE_AND_CONQUER = "--dc";
 
+    private static void printErrorMessageAndExit() {
+        System.err.println("\uD83C\uDFB7 Invalid parameters. Should be path of directory and method (--bf or --dc)");
+        System.exit(1);
+    }
+
+
     public static void main(String[] args) throws IOException {
+
         String path;
+
+        Boolean bruteForce = false;
+
+        Boolean divideAndConquer = false;
+
         if (args.length == 2) {
-
-            if (args[1].equals(BRUTE_FORCE)) {
-                System.out.println("Brute force...");
-            } else if (args[1].equals(DIVIDE_AND_CONQUER)) {
-                System.out.println("Dividing and conquering...");
-            } else {
-                System.err.println("\uD83C\uDFB7 Invalid parameters. Should be path of directory and method (--bf or --dc)");
-                System.exit(1);
-            }
-
-            for (int j = STEP; j < MAX; j += STEP) {
-                path = args[0];
-                File file = new File(path.concat("case-" + j + ".txt"));
-                BufferedInputStream bf = new BufferedInputStream(new FileInputStream(file));
-                Scanner sc = new Scanner(bf);
-
-                int size = Integer.parseInt(sc.nextLine());
-                int[] vector = new int[size];
-
-                for (int i = 0; i < size; i++) {
-                    vector[i] = Integer.parseInt(sc.nextLine());
-                }
-
-                System.out.println("Case: " + j);
-
-                int res = args[1].equals(BRUTE_FORCE) ? MaxSum.bruteForce(vector, size) : MaxSum.divideAndConquer(vector, 0, size - 1);
-
-                System.out.println("Result:\t" + res);
-                System.out.println("Sums:\t" + MaxSum.sums);
-                System.out.println("Comparisons:\t" + MaxSum.comparisons);
-                System.out.println("Total:\t" + MaxSum.comparisons + MaxSum.sums);
-                System.out.println();
+            switch (args[1]) {
+                case BRUTE_FORCE:
+                    System.out.println("Brute force...");
+                    bruteForce = true;
+                    break;
+                case DIVIDE_AND_CONQUER:
+                    System.out.println("Dividing and conquering...");
+                    bruteForce = false;
+                    break;
+                default:
+                    printErrorMessageAndExit();
+                    break;
             }
         } else {
-            System.err.println("\uD83C\uDFB7 Invalid parameters. Should be path of directory and method (--bf or --dc)");
+            printErrorMessageAndExit();
         }
 
+        System.out.println("CASE\tRESULT\tSUMS\tCOMPARISONS\tTOTAL");
+
+        for (int j = STEP; j <= MAX; j += STEP) {
+            path = args[0];
+            File file = new File(path.concat("case-" + j + ".txt"));
+            BufferedInputStream bf = new BufferedInputStream(new FileInputStream(file));
+            Scanner sc = new Scanner(bf);
+
+            int size = Integer.parseInt(sc.nextLine());
+            int[] vector = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                vector[i] = Integer.parseInt(sc.nextLine());
+            }
+            int res = bruteForce ? MaxSum.bruteForce(vector, size) : MaxSum.divideAndConquer(vector, 0, size - 1);
+            System.out.println(j + "\t" + res + "\t" + MaxSum.sums + "\t" + MaxSum.comparisons + "\t" + (MaxSum.comparisons + MaxSum.sums));
+        }
     }
 }
